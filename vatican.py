92c695afee44882d211170d80fa48aed11efd3a1
Nikita Sydorenko <nikita.sydor@gmail.com>
2025-08-17 05:52:47 -0400

    Create vatican.py

---
+++ vatican.py
@@ -0,0 +1,24 @@
+import requests
+import time
+from bot import TelegramAlertBot
+
+URL = "https://tickets.museivaticani.va/api/search/result?lang=it&visitorNum=4&visitDate=18/08/2025&area=1&who=1&page=0"
+bot = TelegramAlertBot()
+
+def check():
+    r = requests.get(URL).json()
+
+    for visit in r["visits"]:
+        if visit["id"] == 640:
+            if visit["availability"] == "SOLD_OUT":
+                bot.send_message(f"Visit '{visit['name']}' is sold out")
+                print(f"Visit '{visit['name']}' is sold out")
+            else:
+                bot.send_message(f"Visit '{visit['name']}' is '{visit['availability']}'")
+                print(f"Visit '{visit['name']}' is '{visit['availability']}'")
+
+
+while True:
+    check()
+    time.sleep(20)
+
